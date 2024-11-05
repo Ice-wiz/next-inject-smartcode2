a5a52ac947e7b7acc7af07797894039bb8572266
aryan <aryan.sharma@vwo.com>
2024-11-05 16:16:27 +0530

    changed boilerplate for smartcode

--- dist/cjs/component.js
+++ dist/cjs/component.js
@@ -86,7 +86,7 @@ var VWOScript = exports.VWOScript = function VWOScript(_ref) {
   var isAppDir = process.env.NEXT_RUNTIME === 'edge' || Boolean(process.env.NEXT_IS_APP_DIR);
 
   // Direct smartcode implementation with configurable parameters
-  var smartCode = "\n    window._vwo_code=window._vwo_code || (function() {\n      var account_id=".concat(accountId, ",\n      version=2.1,\n      settings_tolerance=").concat(settingsTimeout, ",\n      hide_element='").concat(hideElement, "',\n      hide_element_style='").concat(hideElementStyle, "',\n      /* DO NOT EDIT BELOW THIS LINE */\n      f=false,w=window,d=document,v=d.querySelector('#vwoCode'),cK='_vwo_'+account_id+'_settings',cc={};try{var c=JSON.parse(localStorage.getItem('_vwo_'+account_id+'_config'));cc=c&&typeof c==='object'?c:{}}catch(e){}var stT=cc.stT==='session'?w.sessionStorage:w.localStorage;code={use_existing_jquery:function(){return typeof use_existing_jquery!=='undefined'?use_existing_jquery:undefined},library_tolerance:function(){return typeof library_tolerance!=='undefined'?library_tolerance:undefined},settings_tolerance:function(){return cc.sT||settings_tolerance},hide_element_style:function(){return'{'+(cc.hES||hide_element_style)+'}'},hide_element:function(){if(performance.getEntriesByName('first-contentful-paint')[0]){return''}return typeof cc.hE==='string'?cc.hE:hide_element},getVersion:function(){return version},finish:function(e){if(!f){f=true;var t=d.getElementById('_vis_opt_path_hides');if(t)t.parentNode.removeChild(t);if(e)(new Image).src='https://dev.visualwebsiteoptimizer.com/ee.gif?a='+account_id+e}},finished:function(){return f},addScript:function(e){var t=d.createElement('script');t.type='text/javascript';if(e.src){t.src=e.src}else{t.text=e.text}d.getElementsByTagName('head')[0].appendChild(t)},load:function(e,t){var i=this.getSettings(),n=d.createElement('script'),r=this;t=t||{};if(i){n.textContent=i;d.getElementsByTagName('head')[0].appendChild(n);if(!w.VWO||VWO.caE){stT.removeItem(cK);r.load(e)}}else{var o=new XMLHttpRequest;o.open('GET',e,true);o.withCredentials=!t.dSC;o.responseType=t.responseType||'text';o.onload=function(){if(t.onloadCb){return t.onloadCb(o,e)}if(o.status===200){w._vwo_code.addScript({text:o.responseText})}else{w._vwo_code.finish('&e=loading_failure:'+e)}};o.onerror=function(){if(t.onerrorCb){return t.onerrorCb(e)}w._vwo_code.finish('&e=loading_failure:'+e)};o.send()}},getSettings:function(){try{var e=stT.getItem(cK);if(!e){return}e=JSON.parse(e);if(Date.now()>e.e){stT.removeItem(cK);return}return e.s}catch(e){return}},init:function(){if(d.URL.indexOf('__vwo_disable__')>-1)return;var e=this.settings_tolerance();w._vwo_settings_timer=setTimeout(function(){w._vwo_code.finish();stT.removeItem(cK)},e);var t;if(this.hide_element()!=='body'){t=d.createElement('style');var i=this.hide_element(),n=i?i+this.hide_element_style():'',r=d.getElementsByTagName('head')[0];t.setAttribute('id','_vis_opt_path_hides');v&&t.setAttribute('nonce',v.nonce);t.setAttribute('type','text/css');if(t.styleSheet)t.styleSheet.cssText=n;else t.appendChild(d.createTextNode(n));r.appendChild(t)}else{t=d.getElementsByTagName('head')[0];var n=d.createElement('div');n.style.cssText='z-index: 2147483647 !important;position: fixed !important;left: 0 !important;top: 0 !important;width: 100% !important;height: 100% !important;background: white !important;';n.setAttribute('id','_vis_opt_path_hides');n.classList.add('_vis_hide_layer');t.parentNode.insertBefore(n,t.nextSibling)}var o='https://dev.visualwebsiteoptimizer.com/j.php?a='+account_id+'&u='+encodeURIComponent(d.URL)+'&vn='+version;if(w.location.search.indexOf('_vwo_xhr')!==-1){this.addScript({src:o})}else{this.load(o+'&x=true')}}};w._vwo_code=code;code.init();})();(function(){var i=window;function t(){if(i._vwo_code){var e=t.hidingStyle=document.getElementById('_vis_opt_path_hides')||t.hidingStyle;if(!i._vwo_code.finished()&&!_vwo_code.libExecuted&&(!i.VWO||!VWO.dNR)){if(!document.getElementById('_vis_opt_path_hides')){document.getElementsByTagName('head')[0].appendChild(e)}requestAnimationFrame(t)}}}t()})();\n  ");
+  var smartCode = "\n    window._vwo_code=window._vwo_code || (function() {\n      var account_id=".concat(accountId, ",\n      version=2.1,\n      settings_tolerance=").concat(settingsTimeout, ",\n      hide_element='").concat(hideElement, "',\n      hide_element_style='").concat(hideElementStyle, "',\n      /* DO NOT EDIT BELOW THIS LINE */\n      f=false,w=window,d=document,v=d.querySelector('#vwoCode'),cK='_vwo_'+account_id+'_settings',cc={};try{var c=JSON.parse(localStorage.getItem('_vwo_'+account_id+'_config'));cc=c&&typeof c==='object'?c:{}}catch(e){}var stT=cc.stT==='session'?w.sessionStorage:w.localStorage;code={use_existing_jquery:function(){return typeof use_existing_jquery!=='undefined'?use_existing_jquery:undefined},library_tolerance:function(){return typeof library_tolerance!=='undefined'?library_tolerance:undefined},settings_tolerance:function(){return cc.sT||settings_tolerance},hide_element_style:function(){return'{'+(cc.hES||hide_element_style)+'}'},hide_element:function(){if(performance.getEntriesByName('first-contentful-paint')[0]){return''}return typeof cc.hE==='string'?cc.hE:hide_element},getVersion:function(){return version},finish:function(){if(!f){f=true;var t=d.getElementById('_vis_opt_path_hides');if(t)t.parentNode.removeChild(t)}},finished:function(){return f},load:function(){var e=d.createElement('script');e.src='https://dev.visualwebsiteoptimizer.com/j.php?a='+account_id+'&u='+encodeURIComponent(d.URL)+'&f='+(+f);d.getElementsByTagName('head')[0].appendChild(e)},init:function(){if(d.URL.indexOf('__vwo_disable__')>-1)return;var e=this.settings_tolerance();w._vwo_settings_timer=setTimeout(function(){w._vwo_code.finish()},e);var t=d.createElement('style'),i=hide_element?hide_element+'{'+hide_element_style+'}':'',n=d.getElementsByTagName('head')[0];t.setAttribute('id','_vis_opt_path_hides');v&&t.setAttribute('nonce',v.nonce);t.setAttribute('type','text/css');if(t.styleSheet)t.styleSheet.cssText=i;else t.appendChild(d.createTextNode(i));n.appendChild(t);this.load()}};w._vwo_code=code;code.init()})();\n  ");
 
   // Input validation
   if (!accountId) {
@@ -99,25 +99,36 @@ var VWOScript = exports.VWOScript = function VWOScript(_ref) {
   }
   if (type === 'OSC') {
     return /*#__PURE__*/_react["default"].createElement("script", _extends({
+      id: "vwoCode",
+      strategy: "beforeInteractive",
       src: "https://dev.visualwebsiteoptimizer.com/lib/".concat(accountId, ".js")
     }, scriptAttributes));
   }
-  if (isAppDir) {
+
+  // For Page Router, we need to dynamically import Script from next/script
+  if (!isAppDir) {
+    var _Script = require('next/script')["default"];
     return /*#__PURE__*/_react["default"].createElement(_react["default"].Fragment, null, /*#__PURE__*/_react["default"].createElement("link", {
       rel: "preconnect",
       href: "https://dev.visualwebsiteoptimizer.com"
-    }), /*#__PURE__*/_react["default"].createElement("script", _extends({
-      type: "text/javascript",
+    }), /*#__PURE__*/_react["default"].createElement(_Script, _extends({
       id: "vwoCode",
+      strategy: "beforeInteractive",
       dangerouslySetInnerHTML: {
         __html: smartCode
       }
     }, scriptAttributes)));
   }
 
-  // Page Router implementation
-  return /*#__PURE__*/_react["default"].createElement(_react["default"].Fragment, null, /*#__PURE__*/_react["default"].createElement(_script["default"], _extends({
+  // App Router implementation
+  return /*#__PURE__*/_react["default"].createElement(_react["default"].Fragment, null, /*#__PURE__*/_react["default"].createElement("link", {
+    rel: "preconnect",
+    href: "https://dev.visualwebsiteoptimizer.com"
+  }), /*#__PURE__*/_react["default"].createElement("script", _extends({
+    type: "text/javascript",
     id: "vwoCode",
-    strategy: "beforeInteractive"
-  }, scriptAttributes), smartCode));
+    dangerouslySetInnerHTML: {
+      __html: smartCode
+    }
+  }, scriptAttributes)));
 };
